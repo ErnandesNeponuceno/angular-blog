2f1c987c5469a710a7edd60dee7aefd5dd9fdafc
ErnandesNeponuceno <130677901+ErnandesNeponuceno@users.noreply.github.com>
2024-03-22 23:52:33 -0300

    Adicionando novos components

---
+++ src/app/data/data.ts
@@ -0,0 +1,20 @@
+export const data = [
+    {
+       "id":"1",
+       "title": "O Império Contra-Ataca: Luke e Leia Desafiam o Lado Sombrio do Pai em uma Disputa Doméstica Interespacial",
+       "description":"Em uma reviravolta galáctica surpreendente, Darth Vader, o temido Lorde Sith, conhecido por sua crueldade implacável, foi flagrado em uma situação inesperada: lidando com as travessuras de seus dois filhos pequenos, Luke Skywalker e Princesa Leia Organa.",
+       "photoCover":"https://www.acreviewdental.com/wp-content/uploads/placeholder-img-1.jpg"
+    },
+    {
+        "id":"2",
+        "title": "Chefes Imperiais Revelam os Segredos por Trás dos Pratos Mais Deliciosos da Galáxia",
+        "description":"Em uma galáxia repleta de estrelas, planetas e culturas diversas, a culinária intergaláctica floresce como uma expressão única da criatividade e da fusão de sabores. No centro desse universo gastronômico, estão os Chefes Imperiais, mestres culinários que dominam os segredos por trás dos pratos mais deliciosos da galáxia. Em uma jornada para desvendar esses mistérios, mergulhamos nas cozinhas estelares do Império em busca de receitas que transcenderiam os limites da imaginação.",
+        "photoCover":"https://www.acreviewdental.com/wp-content/uploads/placeholder-img-1.jpg"
+     },
+       {
+       "id":"3",
+       "title": "Imperador Palpatine Anuncia Reformas Surpreendentes para Melhorar a Eficiência do Império",
+       "description":"noticia chocante",
+       "photoCover":"https://www.acreviewdental.com/wp-content/uploads/placeholder-img-1.jpg"
+    }
+]
